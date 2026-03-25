fddd670b4c69e9bed9db779a4afbcaf0f63868a8
Umesh <umeshshretha631@gmail.com>
2026-03-25 19:07:57 +0545

    sorting algorithm

---
+++ sorting_algor_comp_math/insertion_sort.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main(){
+    int arr[] = {9 , 12 , 84 ,68 , 0 , 48 , 27 , 84 , 75 , 93};
+    float size_of_arr = sizeof(arr[10]);
+    std::cout<<size_of_arr;
+
+
+
+
+
+    return 0;
+}
